73449c62c4e170c2f18bc6608b5b58dc253e8adc
Michael Smith <smithatlanta@gmail.com>
2020-07-14 10:52:49 -0400

    missed these 2 items in my haste

--- tf_existing_net_rds_ec_ec2/main.tf
+++ tf_existing_net_rds_ec_ec2/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_version = ">=0.12.19"
 
   backend "s3" {
-    bucket = "tf-state-cas-airflow" # the terraform state bucket has to be hand entered unfortunately
+    bucket = "" # the terraform state bucket has to be hand entered unfortunately
     key    = "tf_existing_net_rds_ec_ec2/terraform.tfstate"
     region = "us-east-1"
   }
@@ -13,7 +13,7 @@ terraform {
 provider "aws" {
   version = "~> 2.44.0"
   region  = "us-east-1"
-  profile = "aws-cloudarch-sandbox:aws-cloudarch-sandbox-admin"
+  profile = "" # the profile has to be hand entered unfortunately
 }
 
 provider "random" {
